98da11388f23a865af24b67deff63e543acefc65
bkhazaal <thajukez101@gmail.com>
2024-06-19 09:20:14 -0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-white text-black">
+      <div className="container flex flex-col items-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight transition-all hover:scale-105 sm:text-[5rem]">
+          Welcome!
+        </h1>
+      </div>
+    </main>
+  );
+}
+
+async function CrudShowcase() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const latestPost = await api.post.getLatest();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+    </div>
+  );
+}
