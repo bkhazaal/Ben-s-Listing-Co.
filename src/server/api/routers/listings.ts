713b4258c9fc0c7e688a2b9562cfa457381bcd8b
bkhazaal <thajukez101@gmail.com>
2024-06-21 16:03:29 -0700

    first-item-created

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -1,4 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  const listing1 = await prisma.listing.create({
+    data: {
+      location: "New Jersey",
+      askingPrice: 3000000,
+      grossRevenue: 10000000,
+      adjCashflow: 2000000000,
+    },
+  });
+  console.log(listing1);
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
