395efedbe731219b47538e8c87b61dff416703d7
bkhazaal <thajukez101@gmail.com>
2024-07-01 11:04:48 -0700

    github-generated

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,16 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 
 import { authOptions } from "~/server/auth";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
+
+export const options: AuthOptions = {
+  providers: [],
+  pages: {
+    signIn: "",
+    signOut: "",
+    newUser: "",
+  },
+};
