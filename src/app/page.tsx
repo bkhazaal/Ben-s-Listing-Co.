0757488901fd8e82e32ab27b5b8f1c6000671d8b
bkhazaal <thajukez101@gmail.com>
2025-04-14 08:15:15 -0700

    new name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
 
 export default async function Home() {
   const session = await getServerAuthSession();
-  if (session?.user)
+  const userdata = await api.user.users();
+
+  if (session)
     return (
       <main>
         <h1 className="fadeInUp-animation flex justify-center p-[50px] text-center text-4xl font-bold">
           Welcome
           <br />
-          {session.user?.name}
+          {userdata?.firstName} {userdata?.lastName}
         </h1>
       </main>
     );
